e8baeaca76ec3c957339e546fd700d2f80369ece
Naoki Yano <puni87gou@gmail.com>
2019-01-27 21:57:34 +0900

    Update install_dependencies.sh (#40)

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -1,6 +1,6 @@
 ARD_VER="1.8.7"
 echo "Downloading version $ARD_VER of the Arduino IDE..."
-curl  http://downloads.arduino.cc/arduino-1.8.7-linux64.tar.xz | tar Jxfv -
+curl  http://downloads.arduino.cc/arduino-$ARD_VER-linux64.tar.xz | tar Jxfv -
 git clone --depth 1 https://github.com/jrowberg/i2cdevlib.git
 mv i2cdevlib/Arduino/* arduino-$ARD_VER/libraries
 sudo mv arduino-$ARD_VER /usr/local/share/arduino
