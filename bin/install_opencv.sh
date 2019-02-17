97e3ce2e0983b9511a1fd0a17e85d07b78c088b6
Naoki Yano <puni87gou@gmail.com>
2019-02-17 19:20:53 +0900

    キャッシュを利用する設定にした

--- bin/install_opencv.sh
+++ bin/install_opencv.sh
@@ -2,8 +2,8 @@
 
 
 echo "Installing reqiured packages for OpenCV..."
-sudo apt-get install build-essential
-sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo aptitude install build-essential
+sudo aptitude install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 
 echo "Downloading OpenCV..."
 git clone https://github.com/opencv/opencv.git
