6f82cb408af06379d3b1a70374689cb18ca80170
Naoki Yano <puni87gou@gmail.com>
2019-02-17 20:37:46 +0900

    updated install_opencv.sh

--- bin/install_opencv.sh
+++ bin/install_opencv.sh
@@ -5,7 +5,7 @@ echo "Installing reqiured packages for OpenCV..."
 sudo apt-get install build-essential
 sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 
-if [ ! -e opencv]; then
+if [ ! -e opencv ]; then
 	echo "Cloning OpenCV..."
 	git clone https://github.com/opencv/opencv.git
 fi
