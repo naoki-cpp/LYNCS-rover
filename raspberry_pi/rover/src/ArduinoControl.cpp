1bfb96b39eb959ea946fb2a2dad42a4c7ef6c6d0
Naoki Yano <puni87gou@gmail.com>
2019-03-01 16:50:17 +0900

    Feture/arduino control mod (#64)
    * caseにした
    
    * switch文にした
    
    * returnの追加
    
    * インデントのフォーマット
    
    * returnの追加

--- raspberry_pi/rover/src/ArduinoControl.cpp
+++ raspberry_pi/rover/src/ArduinoControl.cpp
@@ -15,44 +15,45 @@ ArduinoControl::~ArduinoControl()
 {
 }
 
-int ArduinoControl::Init(){
+int ArduinoControl::Init()
+{
 	int ret_cs = csearch_.Init();
 	int ret_ar = transfer_.Init();
-	if (ret_cs < 0 || ret_ar < 0) {
+	if (ret_cs < 0 || ret_ar < 0)
+	{
 		return -1;
-	}else
+	}
+	else
 	{
 		return 0;
 	}
 }
 int ArduinoControl::Transfer(int angle, unsigned char order)
 {
-	transfer_.Transfer(angle,order);
+	transfer_.Transfer(angle, order);
 }
 int ArduinoControl::Csearch1()
 {
 	int judgei;
-	char k = 0;
 	double xy[2];
-
-	while (k < 4)
+	for (int i = 0; i < 4; i++)
 	{
 		judgei = csearch_.Search(118, 117, 122, 119, xy);
-		if (judgei == 2 or judgei == 3)
+		switch (judgei)
 		{
+		case 0:
+			return 1;
+			break;
+		case 2:
+		case 3:
 			transfer_.Transfer(0, 1);
-			cout << "0" << " "<< "1" << endl;
+			return 0;
+			break;
+		default:
 			break;
 		}
-		if (judgei == 0)
-		{
-			return 1;
-			break;
-			return 1;
-		}
-
-		k++;
 	}
+	return 0;
 }
 
 void ArduinoControl::Csearch2()
@@ -61,30 +62,27 @@ void ArduinoControl::Csearch2()
 	char k = 0;
 	double answer;
 	double xy[2];
-
-	while (k < 4)
+	for (int i = 0; i < 4; i++)
 	{
 		judgei = csearch_.Search(10, 0, 180, 140, xy);
-		if (judgei == 2)
+		switch (judgei)
 		{
+		case 0:
+			Transfer(0, 2);
+			return 0;
+			break;
+		case 2:
 			answer = ConvertCoordinateToAngle(xy) * 1000;
-			transfer_.Transfer((int)answer, 4);
-			cout << answer << " " << "4" <<  endl;
+			Transfer((int)answer, 4);
+			return 0;
+			break;
+		case 3:
+			Transfer(0, 3);
+			return 0;
+			break;
+		default:
 			break;
 		}
-		if (judgei == 0)
-		{
-			transfer_.Transfer(0, 2);
-			cout << "0" << " "<< "2" << endl;
-			break;
-		}
-		if (judgei == 3)
-		{
-			transfer_.Transfer(0, 3);
-			cout << "0" << " "<< "3" << endl;
-			break;
-		}
-
-		k++;
 	}
+	return 0;
 }
